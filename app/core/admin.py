c0a2bc82dd33533f92bfaf6b5ef8634c2d571bf5
Lawal Oladotun <oladotunlawal7@gmail.com>
2025-01-07 00:05:25 +0000

    customizing django admin

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,45 @@
-from django.contrib import admin  # noqa
+"""
+Django Admin Customization
+"""
 
-# Register your models here.
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import (
+    gettext_lazy as _,
+)  # this helps with language translation
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for users"""
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    # We define the fieldsets here to match what we have in our custom user model. Check this 'test_edit_user_page' test
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Permissions"), {"fields": ("is_active", "is_staff", "is_superuser")}),
+        (_("Important Dates"), {"fields": ("last_login",)}),
+    )
+    readonly_fields = ["last_login"]
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "name",
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(models.User, UserAdmin)
